4d745e3d52b2695921a71ec07a7673692d33d22a
alfredosetiawijaya-code <alfredosetiawijaya@gmail.com>
2026-09-02 15:46:28 +0700

    week01: refactor to use val and string templates

--- src/week01/GradingSystem.kt
+++ src/week01/GradingSystem.kt
@@ -2,8 +2,9 @@ package week01
 
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    val name: String = "John Thor"
+    val score: Int = 80
 
     println("Nama:" + name + " Nilai " + score)
+
 }
