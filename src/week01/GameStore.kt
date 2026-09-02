7213416a6b0bfa3ea00ee6d9e2cf9120a971a762
alfredosetiawijaya-code <alfredosetiawijaya@gmail.com>
2026-09-02 16:22:12 +0700

    week01: (tugas 2) implement discount logic

--- src/week01/GameStore.kt
+++ src/week01/GameStore.kt
@@ -2,8 +2,18 @@ package week01
 
 fun main() {
     val gameTitle = "Minecraf"
-    val price= 500000
+    val price= 600000
 
     println("Game: $gameTitle")
     println("Harga: Rp $price")
+
+    val discount = calculateDiscount(price)
+    println("Diskon: Rp $discount")
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 500000) {
+        price * 0.20
+    } else {
+        price * 0.10
+    }
