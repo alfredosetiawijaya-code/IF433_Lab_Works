fbe8348362a8b8cdf77f9d2e4618a78dcc96cf68
alfredosetiawijaya-code <alfredosetiawijaya@gmail.com>
2026-09-16 22:27:52 +0700

    week03: add private property

--- src/week3/Employee.kt
+++ src/week3/Employee.kt
@@ -1,13 +1,25 @@
 package week3
 
-class Employee(val name: String){
+class Employee(val name: String) {
+
     var salary: Int = 0
-    set(value) {
-        if (value < 0){
-            println("ERROR:Gaji tidak boleh negatif! Di set ke 0")
-            field = 0
-        } else  {
-            field = value
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di set ke 0")
+                field = 0
+            } else {
+                field = value
+            }
         }
+
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
     }
 }
