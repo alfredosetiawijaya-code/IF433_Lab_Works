5e7e284962adbf03f55cf69b7f2abb47777321e5
alfredosetiawijaya-code <alfredosetiawijaya@gmail.com>
2026-09-16 22:35:49 +0700

    week03: test all encapsulation logic in main

--- src/week3/Employee.kt
+++ src/week3/Employee.kt
@@ -22,4 +22,6 @@ class Employee(val name: String) {
     fun printStatus() {
         println("Karyawan: $name, Rating: $performanceRating")
     }
+    val tax: Double
+        get() = salary * 0.1
 }
--- src/week3/Main.kt
+++ src/week3/Main.kt
@@ -1,5 +1,11 @@
+import week3.Employee
+
 fun main() {
     val e = Employee("Budi")
-    println("Test Error")
-    e.salary = 5000
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+    e.incresesPerformance()
+
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
