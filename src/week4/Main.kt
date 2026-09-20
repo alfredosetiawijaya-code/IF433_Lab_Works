c8f08502b3a9f9c7b50a0c0fbe3ce614fb893138
alfredosetiawijaya-code <alfredosetiawijaya@gmail.com>
2026-09-20 19:12:02 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/week4/Main.kt
+++ src/week4/Main.kt
@@ -1,11 +1,15 @@
 package week04
 
 import week4.Car
+import week4.Developer
+import week4.ElectricCar
+import week4.Manager
 import week4.Vehicle
 
 fun main() {
     val vehicle = Vehicle("Toyota")
     val car = Car("Honda", 4)
+    val electricCar = ElectricCar("Tesla", 4, 85)
 
     vehicle.honk()
     vehicle.accelerate()
@@ -15,4 +19,23 @@ fun main() {
     car.honk()
     car.accelerate()
     car.openTrunk()
+
+    println()
+
+    electricCar.accelerate()
+    electricCar.honk()
+    electricCar.openTrunk()
+
+    println()
+
+    val manager = Manager("Budi", 10000000)
+    val developer = Developer("Andi", 8000000, "Kotlin")
+
+    manager.work()
+    println("Bonus Manager: Rp${manager.calculateBonus()}")
+
+    println()
+
+    developer.work()
+    println("Bonus Developer: Rp${developer.calculateBonus()}")
 }
