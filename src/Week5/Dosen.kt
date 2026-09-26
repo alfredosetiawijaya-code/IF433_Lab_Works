ebbf0b7f48415d5a12830db835381126537dbf66
alfredosetiawijaya-code <alfredosetiawijaya@gmail.com>
2026-09-26 21:34:11 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/Week5/Dosen.kt
@@ -0,0 +1,12 @@
+package oop_<nim>_<nama>.week05
+
+class Dosen(nama: String) : Pegawai(nama) {
+
+    override fun bekerja() {
+        println("$nama sedang mengajar mahasiswa.")
+    }
+
+    fun mengajar() {
+        println("$nama sedang melakukan kegiatan mengajar.")
+    }
+}
