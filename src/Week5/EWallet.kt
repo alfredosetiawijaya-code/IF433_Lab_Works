a4d87ddcc9b8ca839ea87b5fa0c9cedde5429bf6
alfredosetiawijaya-code <alfredosetiawijaya@gmail.com>
2026-09-26 21:42:26 +0700

    week05: (task 2) create EWallet class

---
+++ src/Week5/EWallet.kt
@@ -0,0 +1,23 @@
+package week05
+
+class EWallet(
+    accountName: String,
+    var balance: Double
+) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil menggunakan EWallet.")
+            println("Sisa saldo: $balance")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top Up berhasil sebesar $amount")
+        println("Saldo sekarang: $balance")
+    }
+}
